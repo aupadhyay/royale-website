9e46c7ad847ff81e3afa898398e2e8835120dee5
Abhi Upadhyay <abhi.upadhyay01@gmail.com>
2016-02-15 15:44:02 -0800

    initial

---
+++ shop.php
@@ -0,0 +1,14 @@
+<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="KV22T4GUNFPCJ">
+<table>
+<tr><td><input type="hidden" name="on0" value="Sizes">Sizes</td></tr><tr><td><select name="os0">
+	<option value="Test">Test </option>
+	<option value="Test">Test </option>
+	<option value="test">test </option>
+</select> </td></tr>
+</table>
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+4342562182173334 08/2016 052
